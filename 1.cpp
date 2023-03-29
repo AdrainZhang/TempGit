ae2e71b87410b055c469aa88c3ec1fa5b6c92cae
adrian <707417174@qq.com>
2023-03-29 09:58:00 +0800

    提交一个cpp

---
+++ 1.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <stdlib.h>
+int i = 0;
+int main(){
+    i = 4;
+    printf("hello vscode debug!");
+    printf("hello vscode debug!");
+    printf("hello vscode debug!");
+    printf("hello vscode debug!");
+    system("pause");
+    return 0;
+}
